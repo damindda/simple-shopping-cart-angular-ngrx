3cb40b50a40e2bc1d5b66b8497d61bb306c67f82
damindda <damindda@gmail.com>
2022-01-15 21:22:37 +0000

    input filter search changes

--- src/app/components/products/product-search-input/product-search-input.component.ts
+++ src/app/components/products/product-search-input/product-search-input.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  BehaviorSubject,
+  debounceTime,
+  distinctUntilChanged,
+  filter
+} from 'rxjs';
 
 @Component({
   selector: 'app-product-search-input',
-  templateUrl: './product-search-input.component.html'
+  templateUrl: './product-search-input.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductSearchInputComponent implements OnInit {
+
+  keyword$ = new BehaviorSubject<string>('');
 
   constructor() { }
 
   ngOnInit(): void {
+    this.keyword$
+    .pipe(
+      filter((text) => !text || text?.length > 2),
+      debounceTime(1000),
+      distinctUntilChanged(),
+    )
+    .subscribe((data) => {
+      this.keyword$.next(data);
+      console.log('value', data);
+    });
   }
 
-  getSearchedResults() {
+  getSearchedResults(text: string) {
+    this.keyword$.next(text);
   }
 
 }
