b1b2586fef124eafa7161373a666a9d144a34bf0
damindda <damindda@gmail.com>
2022-01-15 21:02:50 +0000

    404 page routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -20,6 +21,10 @@ const routes: Routes = [
       import('./modules/login/login.module').then(
         (module) => module.LoginModule
       ),
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
   }
 ];
 
