dd0cf1451bd4fca25ebe2d9d9d1c0a76278f61ae
damindda <damindda@gmail.com>
2022-01-16 07:06:11 +0000

    header changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,14 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  signOut() {
+    console.log('sign out clicked');
   }
 
 }
