55ac3cd4e0c9ab5aca5d218ef64900e1494fcae3
damindda <damindda@gmail.com>
2022-01-16 08:59:19 +0000

    action types added

---
+++ src/app/store/products/action-types.ts
@@ -0,0 +1,10 @@
+export enum ProductsActionTypes {
+  START_LOADING = '[LOADING] Start Loading',
+  STOP_LOADING = '[LOADING] Stop Loading',
+  GET_ALL_PRODUCTS = '[PRODUCTS PAGE] Get All Products',
+  GET_ALL_PRODUCTS_SUCCESS = '[PRODUCTS PAGE] Products Load Success',
+  GET_ALL_PRODUCTS_ERROR = '[PRODUCTS PAGE] Products Load Error',
+  UPDATE_PRODUCTS = '[PRODUCTS PAGE] Update Products',
+  UPDATE_PRODUCTS_SUCCESS = '[PRODUCTS PAGE] Update Products Success',
+  PAGE_COUNTER = '[PAGE COUNTER] Increment Counter'
+}
