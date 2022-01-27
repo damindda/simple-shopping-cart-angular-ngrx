9b12cec94371e6073ec9e3881c218ef53917c307
damindda <damindda@gmail.com>
2022-01-27 11:00:36 +0000

    removed commented codes

--- src/app/directives/check-access/check-access-levels.directive.ts
+++ src/app/directives/check-access/check-access-levels.directive.ts
@@ -8,26 +8,6 @@ export class CheckAccessLevelsDirective {
   @Input("moduleType") moduleType!: string;
   @Input("accessType") accessType!: string;
 
-
-  // permissions={
-  //   “userID”:”John”,
-  //   “entitled”:[{
-  //   “resource”:”ABC”,
-  //   “create”:false,
-  //   “read”:true,
-  //   “delete”:false,
-  //   “edit”:true
-  //   },
-  //   {
-  //   “resource”:”PQR”,
-  //   “create”:true,
-  //   “read”:true,
-  //   “delete”:false,
-  //   “edit”:true
-  //   }
-  //   ]
-  //   }
-
   constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
@@ -36,17 +16,6 @@ export class CheckAccessLevelsDirective {
   }
 
   checkAccess() {
-    // const accessControls: any = this.auth.getAccessControls();
-    // const module: any = accessControls.find(access => access.module_name === this.moduleType);
-    // this.elementRef.nativeElement.style.display = module[this.accessType] ? "block" : "none";
-
-
-    // this.permissions.entitled.filter(res=>{
-    //   if(res.resource=== this.resource){
-    //   res[this.action] ? this.element.nativeElement.style.display=”block”:
-    //   this.element.nativeElement.style.display=”none”
-    //   }
-    //   })
 
   }
 
